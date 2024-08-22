67b23bd22928d76a03422ab8b0c574bbcb3ec99b
José Alisson <josealissonif@gmail.com>
2024-08-22 15:34:51 -0300

    conceitos básicos do vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <MainHeader />
+  <div class="users">
+    <div class="container">
+      <section>
+        <h5 class="title">Lista de usuários</h5>
+        <form @submit="createUser">
+          <input type="text" placeholder="Nome" v-model="form.name" />
+          <input type="email" placeholder="E-mail" v-model="form.email" />
+          <button type="submit">Adicionar</button>
+        </form>
+        <ul>
+          <li v-for="user in users" :key="user.id">
+            <p>{{ user.name }}</p>
+            <small>{{ user.email }}</small>
+            <a class="destroy" @click="destroyUser(user.id)"> X </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import MainHeader from '@/MainHeader.vue'
+import axios from '@/utils/axios'
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+interface User {
+  id: string
+  name: string
+  email: string
 }
-</style>
+export default defineComponent({
+  components: {
+    MainHeader,
+  },
+  data() {
+    return {
+      users: [] as User[],
+      form: {
+        name: '',
+        email: '',
+      },
+    }
+  },
+  created() {
+    this.fetchUsers()
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const { data } = await axios.get('/users')
+        this.users = data
+      } catch (error) {
+        console.warn(error)
+      }
+    },
+    async createUser() {
+      try {
+        const { data } = await axios.post('/users', this.form)
+        this.users.push(data)
+        this.form = {
+          name: '',
+          email: '',
+        }
+      } catch (error) {
+        console.warn(error)
+      }
+    },
+    async destroyUser(id: User['id']) {
+      try {
+        await axios.delete(`/users/${id}`)
+        const userIndex = this.users.findIndex((user) => user.id === id)
+        this.users.splice(userIndex, 1)
+      } catch (error) {
+        console.warn(error)
+      }
+    },
+  },
+})
+</script>
---
+++ src/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Cataline</h1>
+    <ul>
+      <li>Home</li>
+      <li>Contato</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
